c728132b5730ae229f1af5fd20569d795ff64a9a
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-08-04 15:00:34 +0900

    refactor: css 파일명 수정

--- src/pages/SettingProfile/SettingProfile.jsx
+++ src/pages/SettingProfile/SettingProfile.jsx
@@ -1,26 +1,26 @@
 import { NavLink } from 'react-router-dom';
 
-import './SettingProfile.css';
+import styles from './SettingProfile.module.css';
 import BackBtn from '../../components/Button/BackBtn';
 
 function SettingProfile() {
   return (
-    <div className='container'>
-      <div className='section1'>
+    <div className={styles.container}>
+      <div className={styles.section1}>
         <BackBtn />
       </div>
-      <div className='section2'>
-        <div className='circle'> </div>
-        <div className='info'>
-          <p className='profile-txt'>
-            <span className='senior-txt font-light'>시니어</span>
-            <span className='name-txt font-bold'> 김태환 </span>
-            <span className='nim-txt font-bold'>님</span>
+      <div className={styles.section2}>
+        <div className={styles.circle}> </div>
+        <div className={styles.info}>
+          <p className={styles.profileTxt}>
+            <span className={styles.seniorTxt}>시니어</span>
+            <span className={`${styles.nameTxt} font-bold`}> 김태환 </span>
+            <span className={`${styles.nimTxt} font-bold`}>님</span>
           </p>
-          <p className='date-txt font-light'>0000년 00월 00일</p>
+          <p className={styles.dateTxt}>0000년 00월 00일</p>
         </div>
-        <div className='intro-box'>
-          <p className='intro-txt font-light'>
+        <div className={styles.introBox}>
+          <p className={styles.introTxt}>
             잘 부탁드립니다. 요즘 MZ들은 참 부럽네요. ^^
             <br />
             저도 시대에 뒤떨어지지 않으려면 열심히 배워야겠읍니다.
@@ -30,8 +30,8 @@ function SettingProfile() {
             꽃보다 아름다운... 다인씨가 보고싶은 날입니다. *^^*
           </p>
         </div>
-        <div className='section3'>
-          <NavLink className='profile-edit-bt font-bold' to='/senior/profile-edit'>
+        <div className={styles.section3}>
+          <NavLink className={`${styles.profileEditBtn}`} to='/senior/profile-edit'>
             프로필 수정하기
           </NavLink>
         </div>
--- src/pages/SettingProfileEdit/SettingProfileEdit.jsx
+++ src/pages/SettingProfileEdit/SettingProfileEdit.jsx
@@ -1,11 +1,29 @@
-import './SettingProfileEdit.css';
+import styles from './SettingProfileEdit.module.css';
 
 function SettingProfileEdit() {
   return (
-    <div className='container'>
-      <div className='profile-temp'>profile-temp</div>
-      <div className='info'>sdsd</div>
-      <div className='bt'>sdsd</div>
+    <div className={styles.container}>
+      <div className={styles.section1}>section1-test</div>
+      <div className={styles.section2}>
+        <div className={styles.inputWrapper}>
+          <span>이름</span>
+          <input className={styles.nameBox} type='text' placeholder='이름을 입력하세요.' />
+          <span>생년월일</span>
+          <input className={styles.dateBox} type='text' placeholder='년' />
+          <input className={styles.dateBox} type='text' placeholder='월' />
+          <input className={styles.dateBox} type='text' placeholder='일' />
+          <span>자기소개</span>
+          <input className={styles.introBox} type='text' placeholder='자신의 자격증 등을 어필해주세요.' />
+        </div>
+      </div>
+      <div className={styles.section3}>
+        <button type='button' className={styles.unsavedBtn}>
+          저장하지 않기
+        </button>
+        <button type='button' className={styles.savedBtn}>
+          프로필 저장
+        </button>
+      </div>
     </div>
   );
 }
