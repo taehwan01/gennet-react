18d93bdfb9be87deaa4434abc6451479321f1394
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-08-10 13:16:51 +0900

    style: PageBanner component 폰트 추가

--- src/components/PageBanner/PageBanner.jsx
+++ src/components/PageBanner/PageBanner.jsx
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import styles from './PageBanner.module.scss';
 
 function PageBanner({ pageTitle, pageIntro }) {
+  const user = useSelector((state) => state.user);
+
   return (
     <div className={styles['page-banner']}>
-      <h1 className={`${styles['page-title']} font-bold`}>{pageTitle}</h1>
-      <h3 className={`${styles['page-intro']} font-light`}>{pageIntro}</h3>
+      <h1 className={`${styles['page-title']} font-bold ${user.type === 'SENIOR' ? 'font-55pt' : 'font-40pt'}`}>
+        {pageTitle}
+      </h1>
+      <h3 className={`${styles['page-intro']} font-light ${user.type === 'SENIOR' ? 'font-25pt' : 'font-22pt'}`}>
+        {pageIntro}
+      </h3>
     </div>
   );
 }
@@ -17,3 +24,7 @@ PageBanner.propTypes = {
 };
 
 export default PageBanner;
+
+// className={`${styles['main-message']} font-bold ${
+//   user.type === 'SENIOR' ? 'font-30pt' : 'font-22pt'
+// }`}
