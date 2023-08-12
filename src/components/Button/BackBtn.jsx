ddd225d22e5cd48b72161818264c2cedfa8f1096
Monte <monte6198@gmail.com>
2023-08-13 01:54:27 +0900

    feat: back button 기능 추가

--- src/components/Button/BackBtn.jsx
+++ src/components/Button/BackBtn.jsx
@@ -1,11 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import backIcon from '../../assets/images/back-icon.png';
 
 import styles from './BackBtn.module.scss';
 
 function BackBtn() {
+  const navigate = useNavigate();
+  const onClickButton = () => {
+    navigate(-1);
+  };
   return (
-    <button className={styles['back-btn']} type='button'>
-      <img className={styles['back-icon']} src={backIcon} alt='Back Icon logo' />
+    <button
+      onClick={onClickButton}
+      className={styles['back-btn']}
+      type='button'
+    >
+      <img
+        className={styles['back-icon']}
+        src={backIcon}
+        alt='Back Icon logo'
+      />
     </button>
   );
 }
